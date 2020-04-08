c0a457af4f4caeec080ec9f3705059f14649d905
vahiidrah <vahiid_rah@example.com>
2020-04-08 23:03:54 +0430

    Updates

---
+++ precache-manifest.9a9ce55c6d3f07fda69f4ec8b16cc836.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da51a1caefc2371f5b12018bab62fed8",
+    "url": "/maktabkhooneh-resume/index.html"
+  },
+  {
+    "revision": "226a5e16494f318921b3",
+    "url": "/maktabkhooneh-resume/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "94f004eab96064d38105",
+    "url": "/maktabkhooneh-resume/static/css/main.6959e45a.chunk.css"
+  },
+  {
+    "revision": "226a5e16494f318921b3",
+    "url": "/maktabkhooneh-resume/static/js/2.7a6863cb.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/maktabkhooneh-resume/static/js/2.7a6863cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f004eab96064d38105",
+    "url": "/maktabkhooneh-resume/static/js/main.e8c4378c.chunk.js"
+  },
+  {
+    "revision": "310456156f0d2a6068b5",
+    "url": "/maktabkhooneh-resume/static/js/runtime-main.289f3635.js"
+  }
+]);
